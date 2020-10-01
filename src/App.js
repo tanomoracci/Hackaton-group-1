df534a3aa5da2943290e2cb219c96f0b8a06ea64
Rui Molefas <molefas@gmail.com>
2020-10-01 20:03:01 +0100

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router';
 import { Home } from './pages/Home';
-import { Room } from './pages/Room';
 
 class App extends Component {
   render() {
     return (
       <Switch key="routes">
         <Route exact path="/" component={Home} />
-        <Route path="/room" component={Room} />
       </Switch>
     );
   }
